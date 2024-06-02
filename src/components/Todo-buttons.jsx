90b7e219bd2f753a8695c7b74110804824b89e32
Tim-Elesh <tima.yelivanov@gmail.com>
2024-06-02 12:10:58 +0500

    add input[type=checkbox] in Todo-task-list.jsx

--- src/components/Todo-buttons.jsx
+++ src/components/Todo-buttons.jsx
@@ -3,7 +3,7 @@ import Sort from "./Todo-sort";
 
 function Buttons(){
     return(
-        <div className='todo-buttons flex justify-between bg-white p-5'>
+        <div className='todo-buttons flex justify-between text-gray-400 bg-white p-5'>
             <Counter/>
             <Sort/>
             <button>Clear Completed</button>
--- src/components/Todo-task-list.jsx
+++ src/components/Todo-task-list.jsx
@@ -1,12 +1,30 @@
 function TaskList(){
     return(
         <ul className="todo-task-list text-xl mt-6">
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem, ipsum dolor.</li>
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem ipsum dolor sit amet.</li>
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem ipsum dolor sit.</li>
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem ipsum dolor sit.</li>
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem, ipsum dolor.</li>
-            <li className="list-component my-1 bg-white p-3 py-4">Lorem ipsum dolor sit amet.</li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox" checked/>
+                <p>lorem, ipsum dolor.</p>
+            </li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox"/>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox"/>
+                <p>Lorem ipsum dolor sit.</p>
+            </li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox"/>
+                <p>Lorem ipsum dolor sit.</p>
+            </li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox"/>
+                <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li className="list-component my-1 bg-white p-3 py-4">
+                <input type="checkbox"/>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </li>
         </ul>
     )
 }
