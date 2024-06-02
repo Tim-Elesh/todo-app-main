317eef32eb26c5e50372ef27e5e47a401afa03f5
Tim-Elesh <tima.yelivanov@gmail.com>
2024-06-02 23:03:09 +0500

    styling and add html in jsx elements

--- src/components/Todo-create.jsx
+++ src/components/Todo-create.jsx
@@ -1,8 +1,9 @@
 function Create(){
     return(
         <div className='todo-create text-xl'>
-          <label>
-            <input type='text' placeholder='Create a new todo...'></input>
+          <label className="flex bg-white h-16 justify-between px-3">
+            <input className="" type="checkbox"></input>
+            <input className="rounded" type='text' placeholder='Create a new todo...'></input>
           </label>
         </div>
     )
--- src/components/Todo-sort.jsx
+++ src/components/Todo-sort.jsx
@@ -1,9 +1,9 @@
 function Sort(){
     return(
-        <div className='todo-sort justify-center'>
-            <button className="All-button">All</button>
-            <button className="Active-button">Active</button>
-            <button className="Complete-button">Completed</button>
+        <div className='todo-sort justify-center pl-6 rounded-md'>
+            <button className="All-button pr-2 focus:text-blue-600">All</button>
+            <button className="Active-button px-1 focus:text-blue-600">Active</button>
+            <button className="Complete-button pl-2 focus:text-blue-600">Completed</button>
         </div>
     )
 }
